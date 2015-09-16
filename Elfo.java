8c95dbd219ab0cbecc25b4a22fc3b8ac42800647
Bruno da Silva Verch <bruno_verch@hotmail.com>
2015-09-16 00:31:09 -0300

    classe elfo exercicios 1-2-3

---
+++ Elfo.java
@@ -0,0 +1,46 @@
+public class Elfo{
+
+	private String nome;
+	private int flechas;
+	private static final boolean orelhasgrandes=true;
+	private int experiencia=0;
+
+	//gets e sets
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getFlechas() {
+		return flechas;
+	}
+
+	public void setFlechas(int flechas) {
+		this.flechas = flechas;
+	}
+
+	public int getExperiencia() {
+		return experiencia;
+	}
+
+	//metodos
+	public void atirarFlecha(){
+		experiencia++;
+		flechas--;
+	}
+
+	//construtores
+	public Elfo(String nome){
+		this.nome=nome;
+	}
+
+	public Elfo(String nome, int flechas){
+		this.nome=nome;
+		this.flechas=42;
+	}
+
+
+}
